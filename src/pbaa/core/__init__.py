1da17b7755977ffdd88ac8c10f41f3ee49db4491
dh031200 <imbird0312@gmail.com>
2023-06-16 11:14:13 +0900

    fix reference

--- src/pbaa/core/__init__.py
+++ src/pbaa/core/__init__.py
@@ -1,11 +1,11 @@
 # SPDX-FileCopyrightText: 2023-present dh031200 <imbird0312@gmail.com>
 #
 # SPDX-License-Identifier: Apache-2.0
-from check import check, init
+from .check import check, init
 
 init()
 check()
 
-from grounded_sam import model_init, run  # noqa
+from .grounded_sam import model_init, run  # noqa
 
 __all__ = "model_init", "run"
