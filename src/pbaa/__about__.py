3ff96e9c0472455cd838cfb48dde17780383f3b1
Danny Kim <imbird0312@gmail.com>
2023-06-16 14:12:19 +0900

    Update __about__.py

--- src/pbaa/__about__.py
+++ src/pbaa/__about__.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: 2023-present dh031200 <imbird0312@gmail.com>
 #
 # SPDX-License-Identifier: Apache-2.0
-__version__ = "0.0.1"
+__version__ = "1.0.0"
 
 PYTHON_VERSION_MAJOR, PYTHON_VERSION_MINOR = 3, 10
